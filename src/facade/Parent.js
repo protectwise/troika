850dd6ff4235e982ac1eaad4407d39e215ee9787
Jason Johnston <jason.johnston@protectwise.com>
2016-10-21 14:17:58 -0600

    Handle case where children goes from nonempty to null

--- src/facade/Parent.js
+++ src/facade/Parent.js
@@ -17,9 +17,7 @@ export default class Parent extends FacadeBase {
   }
 
   afterUpdate() {
-    if (this.children) {
-      this.updateChildren(this.children)
-    }
+    this.updateChildren(this.children)
     super.afterUpdate()
   }
 
@@ -27,45 +25,47 @@ export default class Parent extends FacadeBase {
     let oldDict = this._childrenDict || Object.create(null)
     let newDict = this._childrenDict = Object.create(null)
 
-    for (let i = 0, len = children.length; i < len; i++) {
-      let childDesc = children[i]
-      if (!childDesc) continue //child members can be null
-      let key = childDesc.key
-      let cla$$ = childDesc.class
+    if (children) {
+      for (let i = 0, len = children.length; i < len; i++) {
+        let childDesc = children[i]
+        if (!childDesc) continue //child members can be null
+        let key = childDesc.key
+        let cla$$ = childDesc.class
 
-      // Some basic validation in dev mode
-      if (process.env.NODE_ENV !== 'production') {
-        if (!key || !cla$$) {
-          throw 'All scene objects must have a "key" and "class" defined.'
+        // Some basic validation in dev mode
+        if (process.env.NODE_ENV !== 'production') {
+          if (!key || !cla$$) {
+            throw 'All scene objects must have a "key" and "class" defined.'
+          }
+          if (typeof cla$$ !== 'function') {
+            throw 'The "class" property must point to a constructor function.'
+          }
         }
-        if (typeof cla$$ !== 'function') {
-          throw 'The "class" property must point to a constructor function.'
-        }
-      }
 
-      // If a transition/animation is present, upgrade the class to a Animatable wrapper class on demand.
-      // NOTE: changing between animatable/non-animatable results in a full teardown/recreation
-      // of this instance *and its entire subtree*, so try to avoid that by always including the `transition`
-      // definition if the object is expected to ever need transitions, even if it's temporarily empty.
-      let transition = childDesc.transition
-      let animation = childDesc.animation
-      if (transition || animation) {
-        cla$$ = cla$$.$animatableWrapperClass || (cla$$.$animatableWrapperClass = Animatable(cla$$))
-      }
-
-      // If we have an old instance with the same key and class, update it, otherwise instantiate a new one
-      let oldImpl = oldDict[key]
-      let newImpl = oldImpl && (oldImpl.constructor === cla$$) ? oldImpl : new cla$$(this)
-      //always set transition/animation before any other props
-      newImpl.transition = transition
-      newImpl.animation = animation
-      for (let prop in childDesc) {
-        if (childDesc.hasOwnProperty(prop) && !isSpecialDescriptorProperty(prop)) {
-          newImpl[prop] = childDesc[prop]
+        // If a transition/animation is present, upgrade the class to a Animatable wrapper class on demand.
+        // NOTE: changing between animatable/non-animatable results in a full teardown/recreation
+        // of this instance *and its entire subtree*, so try to avoid that by always including the `transition`
+        // definition if the object is expected to ever need transitions, even if it's temporarily empty.
+        let transition = childDesc.transition
+        let animation = childDesc.animation
+        if (transition || animation) {
+          cla$$ = cla$$.$animatableWrapperClass || (cla$$.$animatableWrapperClass = Animatable(cla$$))
         }
+
+        // If we have an old instance with the same key and class, update it, otherwise instantiate a new one
+        let oldImpl = oldDict[key]
+        let newImpl = oldImpl && (oldImpl.constructor === cla$$) ? oldImpl : new cla$$(this)
+        //always set transition/animation before any other props
+        newImpl.transition = transition
+        newImpl.animation = animation
+        for (let prop in childDesc) {
+          if (childDesc.hasOwnProperty(prop) && !isSpecialDescriptorProperty(prop)) {
+            newImpl[prop] = childDesc[prop]
+          }
+        }
+        newImpl.afterUpdate()
+        newDict[key] = newImpl
       }
-      newImpl.afterUpdate()
-      newDict[key] = newImpl
     }
 
     // Destroy all old child instances that weren't reused
