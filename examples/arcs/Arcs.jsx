713b16d0ebc5e1eb937b0002c124c2b0c3685483
Jason Johnston <jason.johnston@protectwise.com>
2016-10-26 21:19:36 -0600

    Refine data generation to carry forward a random selection of old items. Add exit animations and make the exit/update/enter animations appear sequential.

--- examples/arcs/Arcs.jsx
+++ examples/arcs/Arcs.jsx
@@ -5,9 +5,10 @@ import Arc from './Arc'
 
 const TRANS = {
   duration: 700,
-  easing: 'easeOutExpo'
+  easing: 'easeOutExpo',
+  delay: 200
 }
-
+let idCtr = 0
 
 
 export default React.createClass({
@@ -29,17 +30,31 @@ export default React.createClass({
   },
 
   _randomizeData() {
+    let oldData = this.state.data || {}
     let data = {}
     ;['ne1', 'nw1', 'sw1', 'se1', 'ne2', 'nw2', 'sw2', 'se2'].forEach(key => {
       data[key] = []
+      // carry forward a random selection of previous items
+      if (oldData[key]) {
+        oldData[key].forEach(d => {
+          if (Math.random() > .5) {
+            d.isNew = false
+            data[key].push(d)
+          }
+        })
+      }
       let angle = 0
-      let numArcs = Math.round(Math.random() * 15)
+      let numArcs = Math.max(data[key].length, Math.round(Math.random() * 15))
       let marginAngle = 0.0075
       for (let i = 0; i < numArcs; i++) {
-        data[key].push({
-          startAngle: angle + marginAngle,
-          endAngle: (angle += Math.max(Math.random() * Math.PI / 2 / numArcs, 0.02)) - marginAngle
-        })
+        if (!data[key][i]) {
+          data[key].push({
+            id: idCtr++,
+            isNew: true
+          })
+        }
+        data[key][i].startAngle = angle + marginAngle
+        data[key][i].endAngle = (angle += Math.max(Math.random() * Math.PI / 2 / numArcs, 0.02)) - marginAngle
       }
     })
     this.setState({data})
@@ -56,6 +71,7 @@ export default React.createClass({
   render() {
     let state = this.state
     let {width, height} = this.props
+    let angled = state.cameraAngle != 0
 
     function quadrant(key, baseAngle, baseRadius) {
       return {
@@ -63,13 +79,13 @@ export default React.createClass({
         class: List,
         data: state.data[key] || [],
         template: {
-          key: (d, i) => i,
+          key: (d, i) => d.id,
           class: Arc,
           startAngle: d => baseAngle + d.startAngle,
           endAngle: d => baseAngle + d.endAngle,
           startRadius: baseRadius,
           scaleZ: state.deep ? 20 : .0001,
-          animation: (d, i) => ({
+          animation: (d, i) => (d.isNew ? {
             from: {
               opacity: 0,
               endRadius: baseRadius + 1
@@ -80,8 +96,22 @@ export default React.createClass({
             },
             duration: TRANS.duration,
             easing: TRANS.easing,
-            delay: i * 50
-          }),
+            delay: 200 + i * 50
+          } : null),
+          exitAnimation: {
+            from: {
+              opacity: 1,
+              z: 0,
+              endRadius: baseRadius + 30
+            },
+            to: {
+              opacity: 0,
+              z: angled ? -100 : 0,
+              endRadius: baseRadius + (angled ? 30 : 1)
+            },
+            duration: 500,
+            easing: TRANS.easing
+          },
           transition: {
             startAngle: TRANS,
             endAngle: TRANS,
