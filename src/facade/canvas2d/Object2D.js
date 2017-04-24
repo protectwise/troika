37442608a2454f5893246a21d88f0c8fa86d8c24
Jason Johnston <jason.johnston@protectwise.com>
2017-04-24 14:09:58 -0600

    IE11 fails in Babel's destructuring helper for typed arrays

--- src/facade/canvas2d/Object2D.js
+++ src/facade/canvas2d/Object2D.js
@@ -6,14 +6,14 @@ const IDENTITY_MATRIX = [1, 0, 0, 1, 0, 0]
 
 
 function multiplyMatrices(a, b, target) {
-  let [a0, a1, a2, a3, a4, a5] = a
-  let [b0, b1, b2, b3, b4, b5] = b
+  let a0 = a[0], a1 = a[1], a2 = a[2], a3 = a[3]
+  let b0 = b[0], b1 = b[1], b2 = b[2], b3 = b[3], b4 = b[4], b5 = b[5]
   target[0] = a0 * b0 + a2 * b1
   target[1] = a1 * b0 + a3 * b1
   target[2] = a0 * b2 + a2 * b3
   target[3] = a1 * b2 + a3 * b3
-  target[4] = a0 * b4 + a2 * b5 + a4
-  target[5] = a1 * b4 + a3 * b5 + a5
+  target[4] = a0 * b4 + a2 * b5 + a[4]
+  target[5] = a1 * b4 + a3 * b5 + a[5]
 }
 
 
