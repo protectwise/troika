d3e0d9fe883f464afe08a4449e08ef6686ad5a50
Jason Johnston <jason.johnston@protectwise.com>
2017-07-26 14:28:27 -0600

    Refactor instancing shader upgrades to be applied on demand rather than modifying the base ShaderChunk strings; allows instancing to work in custom shaders.

--- src/facade/threejs/InstancingManager.js
+++ src/facade/threejs/InstancingManager.js
@@ -1,7 +1,7 @@
 import {assign} from '../../utils'
 import {InstancedBufferAttribute, InstancedBufferGeometry} from 'three'
 import Group3DFacade from './Group3D'
-import './InstancingShaderUpgrades'
+import {upgradeVertexShader} from './InstancingShaderUpgrades'
 
 
 const INSTANCE_BATCH_SIZE = 1024 //TODO make this an option?
@@ -162,9 +162,10 @@ class InstancingManager extends Group3DFacade {
     let batchGeometry = this._batchGeometryPool.borrow(geometry)
     batchGeometry.maxInstancedCount = 0
 
-    // Upgrade the material to one with the defines to trigger instancing
+    // Upgrade the material to one with the shader modifications and defines to trigger instancing
     let batchMaterial = Object.create(material)
     batchMaterial.defines = assign({}, batchMaterial.defines, {TROIKA_INSTANCED: ''})
+    batchMaterial.onBeforeCompile = onBeforeCompileBatchMaterial
     batchMaterial.$troikaBatchBase = material
 
     // Create a new mesh object to hold it all
@@ -195,6 +196,20 @@ class InstancingManager extends Group3DFacade {
 }
 
 
+function onBeforeCompileBatchMaterial(shaderInfo) {
+  // this === the batch material
+  let baseMaterial = this.$troikaBatchBase
+  let vertShader = baseMaterial.$troikaUpgradedVertex
+  if (!vertShader || vertShader.original !== shaderInfo.vertexShader) {
+    vertShader = baseMaterial.$troikaUpgradedVertex = {
+      original: shaderInfo.vertexShader,
+      upgraded: upgradeVertexShader(shaderInfo.vertexShader)
+    }
+  }
+  shaderInfo.vertexShader = vertShader.upgraded
+}
+
+
 
 /**
  * Pool for the instancing batch geometries
@@ -218,7 +233,7 @@ class BatchGeometryPool {
       for (let row = 0; row < 3; row++) {
         let attr = new InstancedBufferAttribute(new Float32Array(INSTANCE_BATCH_SIZE * 4), 4)
         attr.dynamic = true
-        batchGeometry.attributes[`troikaInstanceModelMatrixRow${row}`] = attr
+        batchGeometry.attributes[`troika_modelMatrixRow${row}`] = attr
         batchGeometry._instanceMatrixAttrs.push(attr) //for quicker lookup
       }
       pool.geometries.push(batchGeometry)
@@ -247,7 +262,7 @@ class BatchGeometryPool {
           if (firstFree > 0) {
             let instancingAttrs = {}
             for (let row = 0; row < 3; row++) {
-              let name = `troikaInstanceModelMatrixRow${row}`
+              let name = `troika_modelMatrixRow${row}`
               instancingAttrs[name] = geometries[i].attributes[name]
             }
           }
--- src/facade/threejs/InstancingShaderUpgrades.js
+++ src/facade/threejs/InstancingShaderUpgrades.js
@@ -1,16 +1,8 @@
 import {ShaderChunk} from 'three'
 
-
-function hasTroikaUpgrade(chunk) {
-  return chunk.indexOf('TROIKA_INSTANCED') > -1
-}
-
-// Upgrade first common vertex shader chunk to add support for the instanced matrices
-if (!hasTroikaUpgrade(ShaderChunk.uv_pars_vertex)) {
-ShaderChunk.uv_pars_vertex += `
-#ifdef TROIKA_INSTANCED
-
-// matrix inversion utility - credit https://github.com/stackgl/glsl-inverse
+const declarations = `
+#if __VERSION__ < 300
+// matrix inversion utility for pre-ES3 - credit https://github.com/stackgl/glsl-inverse
 mat3 inverse(mat3 m) {
   float a00 = m[0][0], a01 = m[0][1], a02 = m[0][2];
   float a10 = m[1][0], a11 = m[1][1], a12 = m[1][2];
@@ -22,67 +14,91 @@ mat3 inverse(mat3 m) {
 
   float det = a00 * b01 + a01 * b11 + a02 * b21;
 
-  return mat3(b01, (-a22 * a01 + a02 * a21), (a12 * a01 - a02 * a11),
-              b11, (a22 * a00 - a02 * a20), (-a12 * a00 + a02 * a10),
-              b21, (-a21 * a00 + a01 * a20), (a11 * a00 - a01 * a10)) / det;
+  return mat3(
+    b01, (-a22 * a01 + a02 * a21), (a12 * a01 - a02 * a11),
+    b11, (a22 * a00 - a02 * a20), (-a12 * a00 + a02 * a10),
+    b21, (-a21 * a00 + a01 * a20), (a11 * a00 - a01 * a10)
+  ) / det;
 }
+#endif
 
 // define attributes for instance matrices
-attribute vec4 troikaInstanceModelMatrixRow0;
-attribute vec4 troikaInstanceModelMatrixRow1;
-attribute vec4 troikaInstanceModelMatrixRow2;
-
-mat4 getTroikaInstanceModelMatrix() {
-  return mat4(
-    troikaInstanceModelMatrixRow0.x, troikaInstanceModelMatrixRow1.x, troikaInstanceModelMatrixRow2.x, 0.0,
-    troikaInstanceModelMatrixRow0.y, troikaInstanceModelMatrixRow1.y, troikaInstanceModelMatrixRow2.y, 0.0,
-    troikaInstanceModelMatrixRow0.z, troikaInstanceModelMatrixRow1.z, troikaInstanceModelMatrixRow2.z, 0.0,
-    troikaInstanceModelMatrixRow0.w, troikaInstanceModelMatrixRow1.w, troikaInstanceModelMatrixRow2.w, 1.0
-  );
-}
-
-mat4 getTroikaInstanceModelViewMatrix() {
-  return viewMatrix * getTroikaInstanceModelMatrix();
-}
-
-#endif
+attribute vec4 troika_modelMatrixRow0;
+attribute vec4 troika_modelMatrixRow1;
+attribute vec4 troika_modelMatrixRow2;
 `
-}
 
 
-// Normal matrix gets calculated per vertex in instancing mode, to avoid the
-// expensive CPU calculation and an extra 3 attributes
-if (!hasTroikaUpgrade(ShaderChunk.defaultnormal_vertex)) {
-ShaderChunk.defaultnormal_vertex = `
-#ifdef TROIKA_INSTANCED
+const modelMatrixVariable = `
+mat4 troika_modelMatrix = mat4(
+  %0.x, %1.x, %2.x, 0.0,
+  %0.y, %1.y, %2.y, 0.0,
+  %0.z, %1.z, %2.z, 0.0,
+  %0.w, %1.w, %2.w, 1.0
+);
+`.replace(/%/g, 'troika_modelMatrixRow')
 
-// from base chunk:
-#ifdef FLIP_SIDED
-objectNormal = -objectNormal;
-#endif
-
-vec3 transformedNormal = transpose(inverse(mat3(getTroikaInstanceModelViewMatrix()))) * objectNormal;
-
-#else
-${ShaderChunk.defaultnormal_vertex}
-#endif
+const modelViewMatrixVariable = `
+mat4 troika_modelViewMatrix = viewMatrix * troika_modelMatrix;
 `
-}
+
+const normalMatrixVariable = `
+mat3 troika_normalMatrix = transpose(inverse(mat3(troika_modelViewMatrix)));
+`
 
 
-// For any other shader chunks that use the modelMatrix and modelViewMatrix uniforms,
-// allow them to use the troika instancing matrices instead:
-let _modelViewRE = /\bmodelViewMatrix\b/gm
-let _modelRE = /\bmodelMatrix\b/gm
-Object.keys(ShaderChunk).forEach(name => {
-  let chunk = ShaderChunk[name]
-  if (!hasTroikaUpgrade(chunk) && (_modelViewRE.test(chunk) || _modelRE.test(chunk))) {
-    ShaderChunk[name] = `
-#ifdef TROIKA_INSTANCED
-${ chunk.replace(_modelViewRE, 'getTroikaInstanceModelViewMatrix()').replace(_modelRE, 'getTroikaInstanceModelMatrix()') }
-#else
-${ chunk }
-#endif
-    `
+const voidMainRE = /\bvoid\s+main\s*\(\s*\)\s*{/gm
+const modelMatrixRE = /\bmodelMatrix\b/gm
+const modelViewMatrixRE = /\bmodelViewMatrix\b/gm
+const normalMatrixRE = /\bnormalMatrix\b/gm
+
+
+// Copied from threejs WebGLProgram so we can pre-expand the shader includes
+function parseIncludes( source ) {
+  const pattern = /^[ \t]*#include +<([\w\d.]+)>/gm
+  function replace(match, include) {
+    let chunk = ShaderChunk[include]
+    return chunk ? parseIncludes(chunk) : match
   }
-})
+  return source.replace( pattern, replace )
+}
+
+
+
+export function upgradeVertexShader(source) {
+  // Pre-expand includes
+  source = parseIncludes(source)
+
+  let usesModelMatrix = modelMatrixRE.test(source)
+  let usesModelViewMatrix = modelViewMatrixRE.test(source)
+  let usesNormalMatrix = normalMatrixRE.test(source)
+
+  // Inject declarations
+  source = source.replace(voidMainRE, `
+#ifdef TROIKA_INSTANCED
+${ declarations }
+#endif
+
+$&
+
+#ifdef TROIKA_INSTANCED
+${ usesModelMatrix ? modelMatrixVariable : '' }
+${ usesModelViewMatrix ? modelViewMatrixVariable : '' }
+${ usesNormalMatrix ? normalMatrixVariable : '' }
+#endif
+`)
+
+  // Translate uniform references to the instance variables
+  if (usesModelMatrix) {
+    source = source.replace(modelMatrixRE, 'troika_modelMatrix')
+  }
+  if (usesModelViewMatrix) {
+    source = source.replace(modelViewMatrixRE, 'troika_modelViewMatrix')
+  }
+  if (usesNormalMatrix) {
+    source = source.replace(normalMatrixRE, 'troika_normalMatrix')
+  }
+  
+  return source
+}
+
