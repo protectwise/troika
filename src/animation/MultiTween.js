289aa9fc24e2a449e18c524edbe8b9f872dacda7
Jason Johnston <jason.johnston@protectwise.com>
2017-11-07 14:01:03 -0700

    Fix multi-keyframe animations, which were broken by a micro-optimization that caused finished tweens to override values from later active tweens. Still edge-casey here but good enough for the sole AnimatableDecorator consumer.

--- src/animation/MultiTween.js
+++ src/animation/MultiTween.js
@@ -18,15 +18,29 @@ class MultiTween extends Tween {
 
     // Tween the total duration time
     super(null, 0, duration, duration, delay, easing, iterations, direction)
-    this.callback = tweens.length === 1 ? tweens[0].gotoElapsedTime.bind(tweens[0]) : this.syncTweens.bind(this)
+    if (tweens.length === 1) {
+      this.callback = tweens[0].gotoElapsedTime.bind(tweens[0])
+    } else {
+      tweens.sort(endTimeComparator) //sort by end time to ensure proper iteration in syncTweens
+      this.callback = this.syncTweens.bind(this)
+    }
     this.tweens = tweens
   }
 
   syncTweens(time) {
-    for (let i = this.tweens.length; i-- > 0;) {
+    // NOTE: forward iteration is important here so the tweens are evaluated in order
+    // of when they end; that way later tweens will take precedence over earlier ones.
+    // TODO would be nice to ignore tweens past their totalElapsed entirely, but have to
+    // figure out how to do that while ensuring they don't get stuck with a value that is
+    // slightly prior to their end state.
+    for (let i = 0, len = this.tweens.length; i < len; i++) {
       this.tweens[i].gotoElapsedTime(time)
     }
   }
+}
+
+function endTimeComparator(a, b) {
+  return a.totalElapsed - b.totalElapsed
 }
 
 export default MultiTween
