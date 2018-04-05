4c5c28f463368706b09db87619e557dfafa025d6
Jason Johnston <jason.johnston@protectwise.com>
2018-04-05 10:30:31 -0600

    Fix unpredictable state in color interpolation when the start/end inputs are unexpected types

--- src/animation/Interpolators.js
+++ src/animation/Interpolators.js
@@ -9,12 +9,24 @@ export function number(fromValue, toValue, progress) {
 }
 
 export function color(fromValue, toValue, progress) {
-  fromColor.set(fromValue)
-  toColor.set(toValue)
+  setColor(fromColor, fromValue)
+  setColor(toColor, toValue)
   progressColor.setRGB(
     number(fromColor.r, toColor.r, progress),
     number(fromColor.g, toColor.g, progress),
     number(fromColor.b, toColor.b, progress)
   )
   return progressColor.getHex()
+}
+
+function setColor(colorObj, value) {
+  if (value && value.isColor) {
+    colorObj.copy( value )
+  } else if (typeof value === 'number') {
+    colorObj.setHex( value )
+  } else if (typeof value === 'string') {
+    colorObj.setStyle( value )
+  } else {
+    colorObj.setHex(0xffffff)
+  }
 }
