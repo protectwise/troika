4c31b3abd82cc92c64b0f787dc7101ebac7d3761
Jason Johnston <jason.johnston@protectwise.com>
2018-04-05 10:32:03 -0600

    Don't let non-pointing vr controllers invoke mousemove events; fixes flickering hover with two hand controllers

--- src/facade/threejs/vr/VrControllerFacade.js
+++ src/facade/threejs/vr/VrControllerFacade.js
@@ -16,7 +16,9 @@ export default class VrController extends Group3DFacade {
     const now = Date.now()
     if (now - (this._lastRaycast || 0) > raycastFrequency) {
       const ray = this.getPointerRay() //may be null
-      this.notifyWorld('pointerRayMotion', ray)
+      if (ray) {
+        this.notifyWorld('pointerRayMotion', ray)
+      }
       this._lastRaycast = now
     }
   }
