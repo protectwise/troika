41f4aef59d2e76a976feb4337e9f7597f443e8af
Jason Johnston <jason.johnston@protectwise.com>
2018-10-04 15:07:57 -0600

    Update build-time dependencies. Replace uglify command-line with Closure Compiler rollup plugin and minify both outputs during build.

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,59 @@
 // Rollup plugins
 import buble from 'rollup-plugin-buble'
+import closureCompiler from '@ampproject/rollup-plugin-closure-compiler'
 
-export default {
+const external = [
+  'easingjs',
+  'prop-types',
+  'react',
+  'three'
+]
+
+const esmoduleMain = {
   input: 'src/index.js',
   plugins: [
-    // babel({
-    //   exclude: 'node_modules/**',
-    //   runtimeHelpers: true
-    // })
     buble()
   ],
-  external: [
-    'easingjs',
-    'prop-types',
-    'react',
-    'three'
-  ],
-  output: [
-    {
-      format: 'umd',
-      file: 'build/troika.js',
-      name: 'Troika',
-      globals: {
-        easingjs: 'easing',
-        react: 'React',
-        three: 'THREE',
-        'prop-types': 'T'
-      }
-    },
-    {
-      format: 'es',
-      file: 'build/troika.esmodule.js'
-    }
-  ]
+  external,
+  output: {
+    format: 'es',
+    file: 'build/troika.esmodule.js'
+  }
 }
+
+const esmoduleMin = Object.assign({}, esmoduleMain, {
+  plugins: esmoduleMain.plugins.concat(closureCompiler()),
+  output: Object.assign({}, esmoduleMain.output, {file: 'build/troika.esmodule.min.js'})
+})
+
+const umdMain = {
+  input: 'src/index.js',
+  plugins: [
+    buble()
+  ],
+  external,
+  output: {
+    format: 'umd',
+    file: 'build/troika.js',
+    name: 'Troika',
+    globals: {
+      easingjs: 'easing',
+      react: 'React',
+      three: 'THREE',
+      'prop-types': 'T'
+    }
+  }
+}
+
+const umdMin = Object.assign({}, umdMain, {
+  plugins: umdMain.plugins.concat(closureCompiler()),
+  output: Object.assign({}, umdMain.output, {file: 'build/troika.min.js'})
+})
+
+
+export default [
+  esmoduleMain,
+  esmoduleMin,
+  umdMain,
+  umdMin
+]
