0657fa3add747d8b914f342339e8745570c7dd5c
Jason Johnston <jason.johnston@protectwise.com>
2018-10-15 09:56:34 -0600

    WorkerModules: add test suite, and enhance how errors are propagated.

---
+++ packages/troika-core/__tests__/WorkerModules.test.js
@@ -0,0 +1,254 @@
+import 'jsdom-worker'
+import {defineWorkerModule} from '../src/WorkerModules'
+
+
+beforeEach(() => {
+  // Functions that are run in the worker can place breadcrumbs in this object that
+  // `expect` matchers can then retrieve and test later on.
+  global._testDataBucket = {}
+})
+
+
+
+test('requires an `options` argument', () => {
+  expect(() => {
+    defineWorkerModule()
+  }).toThrow()
+})
+
+test('requires an `init` function option', () => {
+  expect(() => {
+    defineWorkerModule({notInit: 'blerp'})
+  }).toThrow()
+  expect(() => {
+    defineWorkerModule({init: 'not_a_function'})
+  }).toThrow()
+  expect(() => {
+    defineWorkerModule({init: () => {}})
+  }).not.toThrow()
+})
+
+test('returns a function', () => {
+  expect(typeof defineWorkerModule({init: () => {}})).toBe('function')
+})
+
+describe('calling the module function', () => {
+  test('throws if init did not return a function', () => {
+    const moduleFn = defineWorkerModule({
+      init: function() {
+        return 'not a function'
+      }
+    })
+    return expect(moduleFn()).rejects.toThrow()
+  })
+
+  test('does not throw if init returns a function', () => {
+    const moduleFn = defineWorkerModule({
+      init: function() {
+        return function() {}
+      }
+    })
+    return expect(moduleFn()).resolves.toBe(undefined)
+  })
+
+  test('returns a thenable', () => {
+    const moduleFn = defineWorkerModule({
+      init: function() {return function() {}}
+    })
+    expect(moduleFn()).toEqual(expect.objectContaining({then: expect.any(Function)}))
+  })
+
+  test('invokes (a clone of) the init function', function() {
+    const moduleFn = defineWorkerModule({
+      init: function() {
+        _testDataBucket.initWasCalled = true
+        return function() {}
+      }
+    })
+    return moduleFn().then(() => {
+      expect(_testDataBucket.initWasCalled).toBe(true)
+    })
+  })
+
+  test('invokes init\'s returned function and resolves the thenable with its return value', () => {
+    const moduleFn = defineWorkerModule({
+      init: function() {
+        return function() {
+          _testDataBucket.initReturnFnWasCalled = true
+          return 'this is the return value'
+        }
+      }
+    })
+    return moduleFn().then((result) => {
+      expect(_testDataBucket.initReturnFnWasCalled).toBe(true)
+      expect(result).toEqual('this is the return value')
+    })
+  })
+
+  test('throws if the init function throws', () => {
+    const moduleFn = defineWorkerModule({
+      init: function() {
+        throw new Error('Expected logged error, please ignore')
+      }
+    })
+    return expect(moduleFn()).rejects.toThrow()
+  })
+})
+
+describe('dependencies', () => {
+  test('are passed to the init function', () => {
+    const moduleFn = defineWorkerModule({
+      dependencies: [
+        1,
+        true,
+        'three'
+      ],
+      init: function(one, two, three) {
+        expect(one).toBe(1)
+        expect(two).toBe(true)
+        expect(three).toBe('three')
+        return function() {}
+      }
+    })
+    return moduleFn()
+  })
+
+  test('can be functions', () => {
+    const moduleFn = defineWorkerModule({
+      dependencies: [
+        function() {
+          _testDataBucket.firstDepFnCalled = true
+        },
+        function(arg1, arg2) {
+          _testDataBucket.secondDepFnArgs = [arg1, arg2]
+        }
+      ],
+      init: function(dep1, dep2) {
+        expect(typeof dep1).toBe('function')
+        expect(typeof dep2).toBe('function')
+        dep1()
+        dep2('hey', 'jude')
+        return function() {}
+      }
+    })
+    return moduleFn().then(() => {
+      expect(_testDataBucket.firstDepFnCalled).toBe(true)
+      expect(_testDataBucket.secondDepFnArgs).toEqual(['hey', 'jude'])
+    })
+  })
+
+  test('can be other worker modules', () => {
+    const depMod1 = defineWorkerModule({
+      dependencies: [123, 456],
+      init: function(n1, n2) {
+        return n1 + n2
+      }
+    })
+    const depMod2 = defineWorkerModule({
+      dependencies: ['abc', 'xyz'],
+      init: function(s1, s2) {
+        return s2 + s1
+      }
+    })
+    const depMod3 = defineWorkerModule({
+      dependencies: [depMod1, depMod2],
+      init: function(d1, d2) {
+        return d1 + d2
+      }
+    })
+
+    const moduleFn = defineWorkerModule({
+      dependencies: [
+        depMod1,
+        depMod2,
+        depMod3
+      ],
+      init: function(dep1, dep2, dep3) {
+        expect(dep1).toEqual(579)
+        expect(dep2).toEqual('xyzabc')
+        expect(dep3).toEqual('579xyzabc')
+        return function() {
+          return [dep1, dep2, dep3]
+        }
+      }
+    })
+    return moduleFn().then((result) => {
+      expect(result).toEqual([579, 'xyzabc', '579xyzabc'])
+    })
+  })
+})
+
+
+describe('getTransferables', () => {
+  test('is called before result is sent to main thread', () => {
+    _testDataBucket.callOrder = []
+
+    const moduleFn = defineWorkerModule({
+      init: function() {
+        _testDataBucket.callOrder.push('init')
+        return function() {
+          _testDataBucket.callOrder.push('module')
+          return {data: new Uint8Array(32)}
+        }
+      },
+      getTransferables: function (result) {
+        _testDataBucket.callOrder.push('getTransferables')
+        return result.data
+      }
+    })
+
+    return moduleFn().then(() => {
+      _testDataBucket.callOrder.push('response')
+    }).then(() => {
+      expect(_testDataBucket.callOrder).toEqual(['init', 'module', 'getTransferables', 'response'])
+    })
+  })
+
+  test('is called with the module return value as param', () => {
+    const moduleFn = defineWorkerModule({
+      init: function() {
+        return function() {
+          return {data: new Uint8Array(32)}
+        }
+      },
+      getTransferables: function (result) {
+        _testDataBucket.getTransferablesParam = result
+        return result.data
+      }
+    })
+
+    return moduleFn().then(() => {
+      expect(_testDataBucket.getTransferablesParam).toEqual({data: expect.any(Uint8Array)})
+    })
+  })
+
+  // TODO not working yet, see comment below
+  test.skip('its return value is used as the `transferables` in postMessage', () => {
+    const moduleFn = defineWorkerModule({
+      init: function() {
+        // Hack: override the worker's postMessage with one we can spy on
+        // TODO this doesn't work, because `init` is invoked in the worker's global scope but
+        // the WorkerModule framework code (that calls postMessage) is wrapped in a narrower
+        // scope by the jsdom-worker polyfill. Will need to find a way around that before this
+        // test can work.
+        const _origPostMessage = postMessage
+        postMessage = function(e, transferList) {
+          _testDataBucket.postMessageTransferList = transferList
+          _origPostMessage(e)
+        }
+
+        return function() {
+          return {data: new Uint8Array(32)}
+        }
+      },
+      getTransferables: function (result) {
+        return [result.data]
+      }
+    })
+
+    return moduleFn().then(() => {
+      expect(_testDataBucket.postMessageTransferList).toEqual([expect.any(Uint8Array)])
+    })
+  })
+})
+
--- packages/troika-core/src/WorkerModules.js
+++ packages/troika-core/src/WorkerModules.js
@@ -13,26 +13,31 @@ openRequests._count = 0
  * interface for moving chunks of logic off the main thread, and managing their dependencies
  * among one another.
  *
- * @param {function} init - The main function that initializes the module. This will be run
+ * @param {object} options
+ * @param {function} options.init - The main function that initializes the module. This will be run
  *        within the worker, and will be passed the resolved dependencies as arguments. Its
  *        return value becomes the module's content, which can then be used by other modules
  *        that depend on it. This function can perform any logic using those dependencies, but
  *        must not depend on anything from its parent closures.
- * @param {array} [dependencies] - Provides any dependencies required by the init function:
+ * @param {array} [options.dependencies] - Provides any dependencies required by the init function:
  *        - Primitives like strings, numbers, booleans
  *        - Raw functions; these will be stringified and rehydrated within the worker so they
  *          must not depend on anything from their parent closures
  *        - Other worker modules; these will be resolved within the worker, and therefore modules
  *          that provide functions can be called without having to cross the worker/main thread
  *          boundary.
- * @param {function} [getTransferables] - An optional function that will be run in the worker
+ * @param {function} [options.getTransferables] - An optional function that will be run in the worker
  *        just before posting the response value from a module call back to the main thread.
  *        It will be passed that response value, and if it returns an array then that will be
  *        used as the "transferables" parameter to `postMessage`. Use this if there are values
  *        in the response that can/should be transfered rather than cloned.
  * @return {function(...[*]): {then}}
  */
-export function defineWorkerModule({dependencies, init, getTransferables}) {
+export function defineWorkerModule(options) {
+  if (!options || typeof options.init !== 'function') {
+    throw new Error('requires `options.init` function')
+  }
+  let {dependencies, init, getTransferables} = options
   const id = `workerModule${++_workerModuleId}`
   let registrationThenable = null
 
@@ -53,21 +58,17 @@ export function defineWorkerModule({dependencies, init, getTransferables}) {
   function moduleFunc(...args) {
     // Register this module if needed
     if (!registrationThenable) {
-      registrationThenable = new BasicThenable()
-      callWorker('registerModule', moduleFunc.workerModuleData, registrationThenable.resolve)
+      registrationThenable = callWorker('registerModule', moduleFunc.workerModuleData)
     }
 
     // Invoke the module, returning a thenable
-    const thenable = new BasicThenable()
-    registrationThenable.then(() => {
-      callWorker('callModule', {
-        id,
-        args
-      }, result => {
-        thenable.resolve(result)
-      })
+    return registrationThenable.then(({isCallable}) => {
+      if (isCallable) {
+        return callWorker('callModule', {id, args})
+      } else {
+        throw new Error('Worker module function was called but `init` did not return a callable function')
+      }
     })
-    return thenable
   }
   moduleFunc.workerModuleData = {
     isWorkerModule: true,
@@ -87,38 +88,46 @@ function getWorker() {
       const modules = Object.create(null)
 
       // Handle messages for registering a module
-      function registerModule({id, dependencies=[], init=function(){}, getTransferables=null}) {
+      function registerModule({id, dependencies=[], init=function(){}, getTransferables=null}, callback) {
         // Only register once
         if (modules[id]) return
 
-        // If any dependencies are modules, ensure they're registered and grab their value
-        dependencies = dependencies.map(dep => {
-          if (dep && dep.isWorkerModule) {
-            registerModule(dep)
-            dep = modules[dep.id].value
+        try {
+          // If any dependencies are modules, ensure they're registered and grab their value
+          dependencies = dependencies.map(dep => {
+            if (dep && dep.isWorkerModule) {
+              registerModule(dep, depResult => {
+                if (depResult instanceof Error) throw depResult
+              })
+              dep = modules[dep.id].value
+            }
+            return dep
+          })
+
+          // Rehydrate functions
+          init = new Function(`return (${init})`)()
+          if (getTransferables) {
+            getTransferables = new Function(`return (${getTransferables})`)()
           }
-          return dep
-        })
 
-        // Rehydrate functions
-        init = new Function(`return (${init})`)()
-        if (getTransferables) {
-          getTransferables = new Function(`return (${getTransferables})`)()
-        }
-
-        // Initialize the module and store its value
-        const value = init(...dependencies)
-        modules[id] = {
-          id,
-          value,
-          getTransferables
+          // Initialize the module and store its value
+          const value = init(...dependencies)
+          modules[id] = {
+            id,
+            value,
+            getTransferables
+          }
+          callback(value)
+        } catch(err) {
+          console.error(err)
+          callback(err)
         }
       }
 
       // Handle messages for calling a registered module's result function
       function callModule({id, args}, callback) {
         if (!modules[id] || typeof modules[id].value !== 'function') {
-          callback(new Error(`Worker module ${id}: not found or did not provide a function`))
+          callback(new Error(`Worker module ${id}: not found or its 'init' did not return a function`))
         }
         try {
           const result = modules[id].value(...args)
@@ -138,21 +147,32 @@ function getWorker() {
             }
             callback(result, tx)
           } catch(err) {
+            console.error(err)
             callback(err)
           }
         }
       }
 
       // Handler for all messages within the worker
-      self.onmessage = e => {
+      self.addEventListener('message', e => {
         const {messageId, action, data} = e.data
         try {
           // Module registration
           if (action === 'registerModule') {
-            registerModule(data)
-            postMessage({
-              messageId,
-              success: true
+            registerModule(data, result => {
+              if (result instanceof Error) {
+                postMessage({
+                  messageId,
+                  success: false,
+                  error: result.message
+                })
+              } else {
+                postMessage({
+                  messageId,
+                  success: true,
+                  result: {isCallable: typeof result === 'function'}
+                })
+              }
             })
           }
           // Invocation
@@ -162,7 +182,7 @@ function getWorker() {
                 postMessage({
                   messageId,
                   success: false,
-                  error: result.stack
+                  error: result.message
                 })
               } else {
                 postMessage({
@@ -180,13 +200,13 @@ function getWorker() {
             error: err.stack
           })
         }
-      }
+      })
     }).toString()
 
     // Create the worker from the bootstrap function content
     worker = new Worker(
       URL.createObjectURL(
-        new Blob([`(${bootstrap})()`], {type: 'application/javascript'})
+        new Blob([`;(${bootstrap})()`], {type: 'application/javascript'})
       )
     )
 
@@ -200,20 +220,23 @@ function getWorker() {
       }
       delete openRequests[msgId]
       openRequests.count--
-      if (response.success) {
-        callback(response.result)
-      } else {
-        console.error(response.error)
-      }
+      callback(response)
     }
   }
   return worker
 }
 
 // Issue a call to the worker with a callback to handle the response
-function callWorker(action, data, callback) {
+function callWorker(action, data) {
+  const thenable = BasicThenable()
   const messageId = ++_messageId
-  openRequests[messageId] = callback
+  openRequests[messageId] = response => {
+    if (response.success) {
+      thenable.resolve(response.result)
+    } else {
+      thenable.reject(new Error(`Error in worker ${action} call: ${response.error}`))
+    }
+  }
   openRequests._count++
   if (openRequests.count > 1000) { //detect leaks
     console.warn('Large number of open WorkerModule requests, some may not be returning')
@@ -223,5 +246,6 @@ function callWorker(action, data, callback) {
     action,
     data
   })
+  return thenable
 }
 
