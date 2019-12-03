ad562b69813ac3418b69f1090f06e5db74ece205
Jason Johnston <jason.johnston@protectwise.com>
2019-12-03 14:35:41 -0700

    XR cursor: scale to constant visual size regardless of distance. Add option for a default distance when not intersecting.

--- packages/troika-xr/src/webxr-wip/facade/CursorFacade.js
+++ packages/troika-xr/src/webxr-wip/facade/CursorFacade.js
@@ -1,8 +1,11 @@
-import { Mesh, MeshBasicMaterial, SphereBufferGeometry } from 'three'
+import { Mesh, MeshBasicMaterial, SphereBufferGeometry, Vector3, Quaternion } from 'three'
 import {Object3DFacade} from 'troika-3d'
 
 const cursorGeom = new SphereBufferGeometry()
 const cursorMaterial = new MeshBasicMaterial({color: 0xffffff})
+const tempVec3 = new Vector3()
+const tempQuat = new Quaternion()
+const degToRadMult = Math.PI / 180
 
 class CursorFacade extends Object3DFacade {
   constructor(parent) {
@@ -11,16 +14,36 @@ class CursorFacade extends Object3DFacade {
       cursorMaterial.clone()
     ))
 
-    // TODO set scale on each intersection to maintain uniform visible size regardless of distance?
-    this.scale = 0.005
+    /**
+     * Visual size at which the cursor should be drawn, regardless of its distance.
+     * This is measured in degrees of the field of view.
+     * @type {number}
+     */
+    this.size = 0.3
+
+    /**
+     * If nonzero, defines the default distance at which the cursor should be displayed when
+     * the targetRay does not intersect any pointable objects in the scene. Defaults to `0`,
+     * which means it is hidden by default.
+     * @type {number}
+     */
+    this.defaultDistance = 0
   }
 
   afterUpdate() {
-    const {rayIntersection} = this
+    const {rayIntersection, defaultDistance, targetRayPose} = this
 
-    // Only display if there is a valid ray intersection
-    if (rayIntersection && rayIntersection.point) {
-      rayIntersection.point.copy.call(this, rayIntersection.point)
+    // Only display if there is a valid ray intersection, or we're configured with a default distance
+    let point = rayIntersection && rayIntersection.point
+    if (!point && defaultDistance && targetRayPose) {
+      point = tempVec3.set(0, 0, -1)
+        .multiplyScalar(defaultDistance) //length
+        .applyQuaternion(tempQuat.copy(targetRayPose.transform.orientation)) //rotation
+        .add(targetRayPose.transform.position) //origin
+    }
+    if (point) {
+      point.copy.call(this, point)
+      this.scale = point.distanceTo(this.getCameraPosition()) * Math.sin(this.size * degToRadMult)
       this.visible = true
     } else {
       this.visible = false
