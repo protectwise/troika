1b07a8fa82cb9d5877ead6309e08f059ffd0da59
Jason Johnston <jason.johnston@protectwise.com>
2020-02-20 10:02:44 -0700

    improvement: enforce conventional commits

---
+++ commitlint.config.js
@@ -0,0 +1,4 @@
+// Git commits must conform to the Conventional Commits format.
+// See https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional
+// The commitlint check will be executed as a git precommit hook to enforce formatting.
+module.exports = {extends: ['@commitlint/config-conventional']};
